e00a25b5dab2e83cc135a090dad26c9ec3bd1fa1
iulianeghina <102993233+iulianeghina@users.noreply.github.com>
2022-10-23 15:45:31 +0200

    Rendered word phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="phonetics">
+      {props.phonetics.map((phonetic, index) => {
+        if (phonetic.text.length > 0 && phonetic.audio.length > 0) {
+          return (
+            <div key={index}>
+              <a href={phonetic.audio} target="_blank">
+                Listen
+              </a>
+              <div>{phonetic.text}</div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
+}
--- src/Result.js
+++ src/Result.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Definition from "./Definition";
 import Synonyms from "./Synonyms";
+import Phonetics from "./Phonetics";
 
 export default function Result(props) {
   if (props.result) {
     return (
       <div className="result">
         <h4>{props.result.word}</h4>
+        <Phonetics phonetics={props.result.phonetics} />
         {props.result.meanings.map((meaning, index) => {
           return (
             <div key={index}>
