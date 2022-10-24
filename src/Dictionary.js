3a7defd8f7b81e743b4c4c6890a97d32043c254d
iulianeghina <102993233+iulianeghina@users.noreply.github.com>
2022-10-24 21:06:43 +0200

    Integrated pictures + improved responsiveness

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,36 @@ import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
 import "./Dictionary.css";
+import Pictures from "./Pictures";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [response, setResponse] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
     setResponse(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000017f3dc78b932547a69b03d12e9c3a7d7c";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let header = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: header,
+      })
+      .then(handlePexelsResponse);
   }
 
   function load() {
@@ -40,7 +57,7 @@ export default function Dictionary(props) {
             autoComplete="off"
             onSubmit={handleSubmit}
           >
-            <div className="col-9">
+            <div className="col-md-9">
               <input
                 className="form-control"
                 type="search"
@@ -49,7 +66,7 @@ export default function Dictionary(props) {
                 onChange={handleChange}
               />
             </div>
-            <div className="col-3">
+            <div className="search-btn col-md-3">
               <button
                 type="submit"
                 className="btn btn-primary"
@@ -64,6 +81,7 @@ export default function Dictionary(props) {
           </p>
         </section>
         <Result result={response} />
+        <Pictures pictures={photos} />
       </div>
     );
   } else {
---
+++ src/Pictures.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Pictures.css";
+
+export default function Pictures(props) {
+  if (props.pictures) {
+    return (
+      <section className="pictures">
+        <div className="row">
+          {props.pictures.map((picture, index) => {
+            return (
+              <div className="col-sm-6 col-md-4" key={index}>
+                <a href={picture.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    className="picture-searched-word img-fluid rounded"
+                    src={picture.src.landscape}
+                    alt="descriptive picture of searched word"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
